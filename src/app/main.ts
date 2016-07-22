0b92019faf37c0aeea60126e54cfb722f06392fc
Lee Blundell <leeblundell16@gmail.com>
2016-07-22 22:23:37 +0100

    Lovely webpack

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,4 +1,10 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+
 import { AppComponent } from './app.component';
 
-bootstrap(AppComponent);
+if (process.env.ENV === 'production') {
+  enableProdMode();
+}
+
+bootstrap(AppComponent, []);
