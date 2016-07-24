8d7a6360af671cd40b2d4292af187dbbcafeb477
Lee Blundell <leeblundell16@gmail.com>
2016-07-24 19:55:09 +0100

    Update git config

--- src/bin/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var express = require('express');
-var compression = require('compression');
-var app = express();
-app.use(compression());
-app.use(express.static(__dirname + '/../public'));
-app.listen(process.env.PORT || 3000);
